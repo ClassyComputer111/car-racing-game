d93c91948a40d7646ac2738f9ef9af4ee73eff15
abhipari444 <75117366+abhipari444@users.noreply.github.com>
2021-04-16 16:09:04 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,36 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+// static is used as keyword here so that we can directly call the get playerInfo function 
+//for all players using class name
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
